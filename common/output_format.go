5df0c383cab6714f6a975a22563153d0e10341ab
Gerardo Lecaros <gelecaro@microsoft.com>
2019-11-04 20:22:13 -0300

    Adding output format

---
+++ common/output_format.go
@@ -0,0 +1,11 @@
+package common
+
+// OutputFormat Defines output formats
+type OutputFormat int
+
+const (
+	// Simple output format
+	Simple   OutputFormat = 0
+	// Detailed output format
+	Detailed OutputFormat = 1
+)
