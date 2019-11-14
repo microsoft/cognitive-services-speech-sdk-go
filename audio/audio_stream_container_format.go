f691ef3f22f419eb7b2766cf4b82dd335bb30ab1
Gerardo Lecaros <gelecaro@microsoft.com>
2019-11-14 16:52:39 -0300

    Adding audio stream container format constants. Adding audio stream format.

---
+++ audio/audio_stream_container_format.go
@@ -0,0 +1,28 @@
+package audio
+
+// AudioStreamContainerFormat defines supported audio stream container format.
+// Changed in version 1.4.0.
+type AudioStreamContainerFormat int
+
+const (
+    // OGGOPUS Stream ContainerFormat definition for OGG OPUS.
+    OGGOPUS AudioStreamContainerFormat = 0x101
+
+    // MP3 Stream ContainerFormat definition for MP3.
+    MP3      AudioStreamContainerFormat = 0x102
+
+    // FLAC Stream ContainerFormat definition for FLAC. Added in version 1.7.0.
+    FLAC     AudioStreamContainerFormat = 0x103
+
+    // ALAW Stream ContainerFormat definition for ALAW. Added in version 1.7.0.
+    ALAW     AudioStreamContainerFormat = 0x104
+
+    // MULAW Stream ContainerFormat definition for MULAW. Added in version 1.7.0.>
+    MULAW    AudioStreamContainerFormat = 0x105
+
+    // AMRNB Stream ContainerFormat definition for AMRNB. Currently not supported.
+    AMRNB    AudioStreamContainerFormat = 0x106
+
+    // AMRWB Stream ContainerFormat definition for AMRWB. Currently not supported.
+    AMRWB    AudioStreamContainerFormat = 0x107
+)
