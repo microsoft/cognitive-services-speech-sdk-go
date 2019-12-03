445b14db1b44de48bff00e231f28bf9753f23c5a
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2019-12-02 16:46:19 -0800

    Adding license.

--- dialog/cfunctions.go
+++ dialog/cfunctions.go
@@ -1,3 +1,7 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
 package dialog
 // This file defines the proxy functions required to use callbacks
 
