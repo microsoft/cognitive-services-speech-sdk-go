cebb4c86c8d69b750e93b881f305b5a658f0d274
Brandon Miller (ACAI) <brandom@microsoft.com>
2020-02-04 13:43:02 -0800

    add kws sample

---
+++ samples/dialog_service_connector/listen_once.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/audio"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/dialog"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/speech"
+	"io"
+	"os"
+	"time"
+)
+
+func pumpFileIntoStream(filename string, stream *audio.PushAudioInputStream) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1000)
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("Done reading file.")
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading file: ", err)
+			break
+		}
+		err = stream.Write(buffer[0:n])
+		if err != nil {
+			fmt.Println("Error writing to the stream")
+		}
+	}
+	stream.CloseStream()
+}
+
+func listenOnce(subscription string, region string, file string) {
+	stream, err := audio.CreatePushAudioInputStream()
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer stream.Close()
+	audioConfig, err := audio.NewAudioConfigFromStreamInput(stream)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer audioConfig.Close()
+	config, err := dialog.NewBotFrameworkConfigFromSubscription(subscription, region)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer config.Close()
+	connector, err := dialog.NewDialogServiceConnectorFromConfig(config, audioConfig)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer connector.Close()
+	sessionStartedHandler := func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Started")
+	}
+	sessionStoppedHandler := func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Stopped")
+	}
+	connector.SessionStarted(sessionStartedHandler)
+	connector.SessionStopped(sessionStoppedHandler)
+	activityReceivedHandler := func(event dialog.ActivityReceivedEventArgs) {
+		defer event.Close()
+		fmt.Println("Received an activity.")
+	}
+	connector.ActivityReceived(activityReceivedHandler)
+	recognizedHandle := func(event speech.SpeechRecognitionEventArgs) {
+		defer event.Close()
+		fmt.Println("Recognized ", event.Result.Text)
+	}
+	connector.Recognized(recognizedHandle)
+	recognizingHandler := func(event speech.SpeechRecognitionEventArgs) {
+		defer event.Close()
+		fmt.Println("Recognizing ", event.Result.Text)
+	}
+	connector.Recognizing(recognizingHandler)
+	pumpFileIntoStream(file, stream)
+	connector.ListenOnceAsync()
+	<-time.After(10 * time.Second)
+}
---
+++ samples/dialog_service_connector/start_keyword_listening.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/audio"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/dialog"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/speech"
+	"time"
+)
+
+func kws(subscription string, region string, file string) {
+	stream, err := audio.CreatePushAudioInputStream()
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer stream.Close()
+	model, err := speech.NewKeywordRecognitionModelFromFile("../../test_files/kws.table")
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+	}
+	defer model.Close()
+	audioConfig, err := audio.NewAudioConfigFromStreamInput(stream)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer audioConfig.Close()
+	config, err := dialog.NewBotFrameworkConfigFromSubscription(subscription, region)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer config.Close()
+	connector, err := dialog.NewDialogServiceConnectorFromConfig(config, audioConfig)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer connector.Close()
+	sessionStartedHandler := func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Started")
+	}
+	sessionStoppedHandler := func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Stopped")
+	}
+	connector.SessionStarted(sessionStartedHandler)
+	connector.SessionStopped(sessionStoppedHandler)
+	activityReceivedHandler := func(event dialog.ActivityReceivedEventArgs) {
+		defer event.Close()
+		fmt.Println("Received an activity.")
+	}
+	connector.ActivityReceived(activityReceivedHandler)
+	recognizedHandle := func(event speech.SpeechRecognitionEventArgs) {
+		defer event.Close()
+		fmt.Println("Recognized ", event.Result.Text)
+	}
+	connector.Recognized(recognizedHandle)
+	recognizingHandler := func(event speech.SpeechRecognitionEventArgs) {
+		defer event.Close()
+		fmt.Println("Recognizing ", event.Result.Text)
+	}
+	connector.Recognizing(recognizingHandler)
+	canceledHandler := func(event speech.SpeechRecognitionCanceledEventArgs) {
+		defer event.Close()
+		fmt.Println("Canceled ", event.Reason)
+		fmt.Println("Error code ", event.ErrorCode)
+	}
+	connector.Canceled(canceledHandler)
+	pumpFileIntoStream(file, stream)
+	connector.StartKeywordRecognitionAsync(model)
+	<-time.After(10 * time.Second)
+}
