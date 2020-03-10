93ca1c4f73ef8e45b374b719011c155722ef9790
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2020-03-10 14:10:53 -0700

    Improve sample code. Make it reference a proper version. (#2)
    * Improve sample code. Make it reference a proper version.
    
    * Fixing tests to work with new bot.
    
    * Adding logging.

---
+++ samples/helpers/stream_utils.go
@@ -0,0 +1,37 @@
+package helpers
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/audio"
+)
+
+func PumpFileIntoStream(filename string, stream *audio.PushAudioInputStream) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1000)
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("Done reading file.")
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading file: ", err)
+			break
+		}
+		err = stream.Write(buffer[0:n])
+		if err != nil {
+			fmt.Println("Error writing to the stream")
+		}
+	}
+	stream.CloseStream()
+}
