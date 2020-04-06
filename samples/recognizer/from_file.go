95bdab520af7e3b6ca407e04fbbcfe37729c3a1e
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2020-04-06 16:24:37 -0700

    Adding SR sample. (#6)
    * Adding SR sample.
    Fixing bug related to SessionStarted event.
    
    * Bumping version for automation to 1.11
    
    * run only on pushes to master.
    
    * fixing tests.

---
+++ samples/recognizer/from_file.go
@@ -0,0 +1,56 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+package recognizer
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/audio"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/speech"
+)
+
+func RecognizeOnceFromWavFile(subscription string, region string, file string) {
+	audioConfig, err := audio.NewAudioConfigFromWavFileInput(file)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer audioConfig.Close()
+	config, err := speech.NewSpeechConfigFromSubscription(subscription, region)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer config.Close()
+	speechRecognizer, err := speech.NewSpeechRecognizerFromConfig(config, audioConfig)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer speechRecognizer.Close()
+	speechRecognizer.SessionStarted(func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Started (ID=", event.SessionID, ")")
+	})
+	speechRecognizer.SessionStopped(func(event speech.SessionEventArgs) {
+		defer event.Close()
+		fmt.Println("Session Stopped (ID=", event.SessionID, ")")
+	})
+	task := speechRecognizer.RecognizeOnceAsync()
+	var outcome speech.SpeechRecognitionOutcome
+	select {
+	case outcome = <-task:
+	case <-time.After(5 * time.Second):
+		fmt.Println("Timed out")
+		return
+	}
+	defer outcome.Close()
+	if outcome.Error != nil {
+		fmt.Println("Got an error: ", outcome.Error)
+	}
+	fmt.Println("Got a recognition!")
+	fmt.Println(outcome.Result.Text)
+}
---
+++ samples/recognizer/from_microphone.go
@@ -0,0 +1,68 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+package recognizer
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/audio"
+	"github.com/Microsoft/cognitive-services-speech-sdk-go/speech"
+)
+
+func sessionStartedHandler(event speech.SessionEventArgs) {
+	defer event.Close()
+	fmt.Println("Session Started (ID=", event.SessionID, ")")
+}
+
+func sessionStoppedHandler(event speech.SessionEventArgs) {
+	defer event.Close()
+	fmt.Println("Session Stopped (ID=", event.SessionID, ")")
+}
+
+func recognizingHandler(event speech.SpeechRecognitionEventArgs) {
+	defer event.Close()
+	fmt.Println("Recognizing:", event.Result.Text)
+}
+
+func recognizedHandler(event speech.SpeechRecognitionEventArgs) {
+	defer event.Close()
+	fmt.Println("Recognized:", event.Result.Text)
+}
+
+func cancelledHandler(event speech.SpeechRecognitionCanceledEventArgs) {
+	defer event.Close()
+	fmt.Println("Received a cancellation: ", event.ErrorDetails)
+}
+
+func ContinuousFromMicrophone(subscription string, region string, file string) {
+	audioConfig, err := audio.NewAudioConfigFromDefaultMicrophoneInput()
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer audioConfig.Close()
+	config, err := speech.NewSpeechConfigFromSubscription(subscription, region)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer config.Close()
+	speechRecognizer, err := speech.NewSpeechRecognizerFromConfig(config, audioConfig)
+	if err != nil {
+		fmt.Println("Got an error: ", err)
+		return
+	}
+	defer speechRecognizer.Close()
+	speechRecognizer.SessionStarted(sessionStartedHandler)
+	speechRecognizer.SessionStopped(sessionStoppedHandler)
+	speechRecognizer.Recognizing(recognizingHandler)
+	speechRecognizer.Recognized(recognizedHandler)
+	speechRecognizer.Canceled(cancelledHandler)
+	speechRecognizer.StartContinuousRecognitionAsync()
+	defer speechRecognizer.StopContinuousRecognitionAsync()
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
