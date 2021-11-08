3c19a6c949b87ef6ed51f4bdeda1fdfca67a91bc
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2021-11-08 09:07:05 -0800

    Bumping version to 1.19.0 (#48)
    * Bumping version to 1.19.0

--- audio/interop_utils.go
+++ audio/interop_utils.go
@@ -4,17 +4,18 @@
 package audio
 
 import (
+	"unsafe"
+
 	"github.com/Microsoft/cognitive-services-speech-sdk-go/common"
 )
 
 // #include <speechapi_c_common.h>
 import "C"
-import "unsafe"
 
 func uintptr2handle(h common.SPXHandle) C.SPXHANDLE {
-	return (C.SPXHANDLE)(unsafe.Pointer(h))
+	return (C.SPXHANDLE)(unsafe.Pointer(h)) //nolint:govet
 }
 
 func handle2uintptr(h C.SPXHANDLE) common.SPXHandle {
-	return (common.SPXHandle)(unsafe.Pointer(h))
+	return (common.SPXHandle)(unsafe.Pointer(h)) //nolint:govet
 }
