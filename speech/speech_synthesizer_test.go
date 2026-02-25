ad8519c0bb4c3172b8ebd8b8c9bcf0c3c194bb8e
Mohamed Zaki <v-mohazaki@microsoft.com>
2026-02-25 18:20:26 +0200

    Fix formatting of duration calculations in TestSynthesizerEvents

--- speech/speech_synthesizer_test.go
+++ speech/speech_synthesizer_test.go
@@ -122,8 +122,8 @@ func TestSynthesizerEvents(t *testing.T) {
 		defer event.Close()
 		t.Logf("SynthesisCompleted, audio length %d", len(event.Result.AudioData))
 		checkSynthesisResult(t, &event.Result, common.SynthesizingAudioCompleted)
-		durationFromProperty := (float64)(event.Result.AudioDuration/time.Millisecond)
-		durationFromAudioBuffer := (float64)(len(event.Result.AudioData)/32)
+		durationFromProperty := (float64)(event.Result.AudioDuration / time.Millisecond)
+		durationFromAudioBuffer := (float64)(len(event.Result.AudioData) / 32)
 		if !almostEqual(durationFromProperty, durationFromAudioBuffer, 150) {
 			t.Errorf("Synthesis duration incorrect (%.2f vs %.2f)", durationFromProperty, durationFromAudioBuffer)
 		}
