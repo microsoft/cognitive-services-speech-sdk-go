30614584bbd0b3f82a40726806bdc89f19b7cf6a
Amber Wang <wangamber@microsoft.com>
2026-07-22 11:33:51 -0700

    Add Live Interpreter speech-to-speech translation sample
    Adds a translation sample demonstrating real-time speech-to-speech translation with Live Interpreter and personal voice (open-range language identification with the universal v2 endpoint). Wired into the sample runner as translation:LiveInterpreterFromFile.

---
+++ samples/translation/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
+// Package translation provides samples of speech translation, including Live Interpreter
+// real-time speech-to-speech translation with personal voice.
+package translation
